41fa5f734c6559adc8ecfc927465fe8d79a860f8
primislas <primislas@protonmail.com>
2026-03-20 02:45:23 +0200

    refactoring

--- ukrainersalis_utils/gemini_translation.py
+++ ukrainersalis_utils/gemini_translation.py
@@ -21,10 +21,13 @@ You are given a text in English and you need to translate it into Ukrainian.
 
 
 class GeminiTranslator(Translator):
-    def __init__(self, model: str = _DEFAULT_GEMINI_MODEL, system_instruction: str = _DEFAULT_SYSTEM_INSTRUCTION):
+    def __init__(self, model: str = _DEFAULT_GEMINI_MODEL, system_instruction: str = _DEFAULT_SYSTEM_INSTRUCTION,
+                 batch_size: int = 50, min_last_batch_size: int = 20):
         self.model = model
         self.system_instruction = system_instruction
         self._gemini: genai.Client | None = None
+        self._batch_size = batch_size
+        self._min_last_batch_size = min_last_batch_size
 
     @override
     def translate(self, text: str) -> str:
@@ -36,6 +39,23 @@ class GeminiTranslator(Translator):
         Returns:
             str: The translated text in Ukrainian.
         """
+
+        lines = text.splitlines()
+        if not lines:
+            return ""
+
+        batches = self._split_into_batches(lines)
+        translations = []
+        for batch in batches:
+            batch_text = "\n".join(batch)
+            translation = self._translate(batch_text)
+            output_lines = self._splitlines_and_pad_to_batch_size(translation, len(batch))
+            translations.extend(output_lines)
+            logger.debug(f"Translated {len(translations)}/{len(lines)} phrases")
+
+        return "\n".join(translations)
+
+    def _get_gemini_client(self) -> genai.Client:
         if self._gemini is None:
             self._gemini = genai.Client(
                 http_options=HttpOptions(
@@ -47,57 +67,50 @@ class GeminiTranslator(Translator):
                     )
                 )
             )
+        return self._gemini
 
-        lines = text.splitlines()
-        if not lines:
-            return ""
-
+    def _split_into_batches(self, lines: list[str]):
         batches = []
-        batch_size = 50
-        min_last_batch_size = 20
 
-        for i in range(0, len(lines), batch_size):
-            batches.append(lines[i:i + batch_size])
+        for i in range(0, len(lines), self._batch_size):
+            batches.append(lines[i:i + self._batch_size])
 
-        if len(batches) > 1 and len(batches[-1]) < min_last_batch_size:
+        if len(batches) > 1 and len(batches[-1]) < self._min_last_batch_size:
             last_batch = batches.pop()
             batches[-1].extend(last_batch)
 
-        translated_batches = []
-        for batch in batches:
-            batch_text = "\n".join(batch)
-            response = self._gemini.models.generate_content(
-                model=self.model,
-                contents=batch_text,
-                config=GenerateContentConfig(system_instruction=self.system_instruction),
-            )
-            
-            input_line_count = len(batch)
-            output_text = response.text
-            output_line_count = output_text.count("\n") + 1
-            if input_line_count > output_line_count:
-                logger.warning(f"Input line count ({input_line_count}) is greater than output line count ({output_line_count})")
-                output_text += "\n" * (input_line_count - output_line_count)
-            elif input_line_count < output_line_count:
-                logger.warning(f"Input line count ({input_line_count}) is less than output line count ({output_line_count})")
-                output_text = output_text.rstrip()
-                output_line_count = output_text.count("\n") + 1
-                if input_line_count > output_line_count:
-                    output_text += "\n" * (input_line_count - output_line_count)
-                elif input_line_count < output_line_count:
-                    output_text = output_text.strip()[:input_line_count]
-            
-            translated_batches.append(output_text)
+        return batches
 
-        return "\n".join(translated_batches)
+    @staticmethod
+    def _splitlines_and_pad_to_batch_size(text: str, batch_size: int) -> list[str]:
+        output_lines = text.splitlines()
+        output_line_count = len(output_lines)
+
+        if output_line_count > batch_size:
+            logger.warning(f"Input line count ({batch_size}) is less than output line count ({output_line_count})")
+            output_lines = output_lines[:batch_size]
+        elif output_line_count < batch_size:
+            logger.warning(f"Input line count ({batch_size}) is greater than output line count ({output_line_count})")
+            output_lines.extend([""] * (batch_size - output_line_count))
+
+        return output_lines
+
+    def _translate(self, text: str) -> str:
+        response = self._get_gemini_client().models.generate_content(
+            model=self.model,
+            contents=text,
+            config=GenerateContentConfig(system_instruction=self.system_instruction),
+        )
+        return response.text
+
 
 
 if __name__ == "__main__":
     load_dotenv()
-    gemini = GeminiTranslator()
-    contents = """$VALUE$
+    _gemini = GeminiTranslator()
+    _contents = """$VALUE$
 PLACEHOLDER
 #color:{0.9,0.1,0.1} $VALUE$#!
 Entries in this layer cannot overlap, so grouping operation with multiple entries is not supported. Please select only one entry."""
-    translation = gemini.translate(contents)
-    logger.info(translation)
+    _translation = _gemini.translate(_contents)
+    logger.info(_translation)
