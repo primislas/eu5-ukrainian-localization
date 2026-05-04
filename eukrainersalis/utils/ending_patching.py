b6cd7736615337a6e26a4f9e69c9ef8eac15fbbe
primislas <primislas@protonmail.com>
2026-05-05 02:28:33 +0300

    * endlong_fem - now properly using -в, -ла, -ло, -ли endings;

---
+++ eukrainersalis/utils/ending_patching.py
@@ -0,0 +1,29 @@
+from eukrainersalis.utils.file_utils import list_localization_files
+from eukrainersalis.utils.translation_utils import Language
+from eukrainersalis.utils.yaml_utils import write_eu5_localization_yaml, load_eu5_yaml
+
+ending_mappings = {
+    "'endlong_fem'": "'endlong_v_la'",
+}
+
+if __name__ == "__main__":
+    # Keeping ending patching separate, to be able to detect them at a glance
+    # with more ease, and to manually patching, as they usually end up
+    # being pretty messy.
+
+    _fixed_declaration = 0
+    _unfixed_dangling_concept = 0
+    _localization_key = Language.RUSSIAN.localization_key
+    for file in list_localization_files("russian_uk_ua_machine_translation"):
+        _content = load_eu5_yaml(file)
+        for k, v in _content.get(_localization_key, {}).items():
+            for ek, ev in ending_mappings.items():
+                if ek in v:
+                    u = v
+                    u = u.replace(ek, ev)
+                    _content[_localization_key][k] = u
+                    if u != v:
+                        _fixed_declaration += 1
+
+        write_eu5_localization_yaml(_content, file)
+    print(f"Fixed {_fixed_declaration} declarations")
